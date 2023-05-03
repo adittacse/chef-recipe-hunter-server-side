2c25f28bc8ea1c01081bd86bbe9f851d78af71ea
Aditta Chakraborty <adittacse@gmail.com>
2023-05-03 19:08:21 +0600

    chef data updated and each chef data collected by using id

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const port = 3000;
+
+app.use(cors());
+
+const chef = require("./data/chefData.json");
+const recipe = require("./data/recipeData.json");
 
 app.get('/', (req, res) => {
   res.send('Chef Recipe Hunter Is Running!');
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get("/chef/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(n => n.id == id);
+    res.send(selectedChef);
+});
+
+app.get('/recipe', (req, res) => {
+    res.send(recipe);
 })
 
 app.listen(port, () => {
