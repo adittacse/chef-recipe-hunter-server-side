63a26a560d1c2a824a8956760ce1f4482635ada4
Aditta Chakraborty <adittacse@gmail.com>
2023-05-04 02:19:07 +0600

    deshi chef title updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const chef = require("./data/chef.json");
 
 app.get('/', (req, res) => {
-  res.send('Chef Recipe Hunter Is Running!');
+  res.send('Deshi Chef Is Running!');
 })
 
 app.get('/chef', (req, res) => {
@@ -26,5 +26,5 @@ app.get('/recipe', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Deshi Chef listening on port ${port}`);
 })
